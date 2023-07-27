c314f2d32d7d7871e13d02c00a228011c2340178
tomik-z-cech <>
2023-07-27 22:11:53 +0000

    Create a list of emojis

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
-import colorama
-from colorama import Fore, Style 
+import colorama, os
+from colorama import Fore, Style
 
-print(Fore.YELLOW + 'Hello, all set up' + Style.RESET_ALL)
+# Emoji list
+NUCLEAR_EMOJI = '\u2622'
+
+
+
+os.system('clear')
+print(Fore.YELLOW + f'{NUCLEAR_EMOJI} Welcome to Fallout Mini - Hangman {NUCLEAR_EMOJI} \n\n' + Style.RESET_ALL)
+
