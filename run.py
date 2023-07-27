61f79e96b026108e61c33c1531a22a9049d16522
tomik-z-cech <>
2023-07-27 22:46:59 +0000

    Create create_character function

--- run.py
+++ run.py
@@ -4,17 +4,34 @@ from colorama import Fore, Style
 
 # Emoji list
 NUCLEAR_EMOJI = '\u2622'
+MAN_EMOJI = '\U0001F466'
+WOMAN_EMOJI = '\U0001F467'
 
 # Global variables
 player_name = ''
+guesses = 0
+lenght_of_word = 0
+letters_revealed = 0
 
-def word_guess(guesses, lenght_of_word):
+
+def word_guess():
     word_guessed = False
     return word_guessed
 
+def create_charater():
+    clear_screen()
+    global player_name 
+    print(Fore.YELLOW + f'{MAN_EMOJI}  Create new character {WOMAN_EMOJI} \n\n' + Style.RESET_ALL)
+    player_name = input("What's your name : ")
+    print(f'Hello, {player_name}')
 
+def clear_screen():
+    os.system('clear')
+    return
 
-os.system('clear')
+clear_screen()
 print(Fore.YELLOW + f'{NUCLEAR_EMOJI} Welcome to Fallout Mini - Hangman {NUCLEAR_EMOJI} \n\n' + Style.RESET_ALL)
-result = word_guess(7, 7)
-print(result)
+result = word_guess()
+# print(result)
+# create_charater()
+# print(player_name)
