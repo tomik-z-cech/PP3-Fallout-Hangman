7345b6f20dd65f602a28b95662b1c944e81c1b44
tomik-z-cech <>
2023-07-28 17:53:41 +0000

    Linked run.py with google sheets

--- run.py
+++ run.py
@@ -1,6 +1,22 @@
 # Imports
-import colorama, os
+import gspread, json, os
+from google.oauth2.service_account import Credentials
 from colorama import Fore, Style
+from datetime import datetime
+
+
+# Google drive credentials
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+creds = json.load(open('creds.json'))
+CREDS = Credentials.from_service_account_info(creds)
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('fallout_hangman')
 
 # Emoji list
 NUCLEAR_EMOJI = '\u2622'
@@ -13,6 +29,15 @@ guesses = 0
 lenght_of_word = 0
 letters_revealed = 0
 
+
+def update_history():
+    """
+    Function updates sheet on google drive with date and time of user access
+    """
+    history_worksheet = SHEET.worksheet("history")
+    time_now = str(datetime.now().time())
+    date_now = str(datetime.now().date())
+    history_worksheet.append_row([date_now,time_now])
 
 def word_guess():
     word_guessed = False
@@ -29,9 +54,13 @@ def clear_screen():
     os.system('clear')
     return
 
-clear_screen()
-print(Fore.YELLOW + f'{NUCLEAR_EMOJI} Welcome to Fallout Mini - Hangman {NUCLEAR_EMOJI} \n\n' + Style.RESET_ALL)
-result = word_guess()
-# print(result)
-create_charater()
-# print(player_name)
+def main():
+    update_history()
+    # clear_screen()
+    print(Fore.YELLOW + f'{NUCLEAR_EMOJI} Welcome to Fallout Mini - Hangman {NUCLEAR_EMOJI} \n\n' + Style.RESET_ALL)
+    # result = word_guess()
+    # print(result)
+    # create_charater()
+    # print(player_name)
+
+main()
